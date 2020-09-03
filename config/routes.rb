155824e850e2befc26b542df4ba37961967ffef0
Peter Jang <peter.x.jang@gmail.com>
2020-09-02 19:21:22 -0500

    Add url segment guess number action

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
     get "/name_path" => "params_examples#name_action"
     get "/query_guess_path" => "params_examples#query_guess_action"
+    get "/segment_guess_path/:my_guess" => "params_examples#query_guess_action"
   end
 end
